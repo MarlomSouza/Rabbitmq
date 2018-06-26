7756f4f7e74fc004eb300c98b4dc4ab115fed54c
Marlom Souza <marlom1012@gmail.com>
2018-06-25 22:11:17 -0400

    Init

---
+++ Send/Send.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = AmqpTcpEndpoint.UseDefaultPort
+            };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    var message = "Hello World Marlom!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(String.Empty, "hello", null, body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
