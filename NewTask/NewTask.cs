883399eff7e15abcae81a51dfde3596dd18f0996
Marlom Souza <marlomsouza@digix.com.br>
2018-06-27 10:18:45 -0400

    exemplo 2

---
+++ NewTask/NewTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NewTask
+{
+    class Program
+    class Program
+    class Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = GetMessage(args);
+            var body = Encoding.UT8.GetBytes(message);
+
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            channel.BasicPublish(exchange: "",
+                                routingKey: "task_queue",
+                                basicProperties: properties,
+                                body: body);      
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
